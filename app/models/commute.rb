360bdbafc7462638bb3df244c157bbfd1ea05f06
Carly Pecora <carly@carlypecora.com>
2019-05-07 13:42:24 -0400

    Done

--- app/models/commute.rb
+++ app/models/commute.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: commutes
+#
+#  id       :integer          not null, primary key
+#  user_id  :integer
+#  train_id :integer
+#
+
 class Commute < ActiveRecord::Base
     belongs_to :user
     belongs_to :train
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id           :integer          not null, primary key
+#  first_name   :string
+#  last_name    :string
+#  neighborhood :string
+#
+
 class User < ActiveRecord::Base
     has_many :commutes
     has_many :trains, through: :commutes
