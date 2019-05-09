8a69b5d9f3660e56ac6c33c23595515133f2855a
Jessica Lin <jessicaa.lin@gmail.com>
2019-05-09 14:24:40 -0400

    Add profile menu, add view your info, update user name and view friends on commute.

--- app/models/user.rb
+++ app/models/user.rb
@@ -75,7 +75,7 @@ class User < ActiveRecord::Base
         puts
         puts "The #{trains_string}?"
         puts
-        response = gets.chomp.downcase
+        response = gets.chomp.upcase
     end
 
     def self.welcome_back(user_obj)
@@ -126,19 +126,84 @@ class User < ActiveRecord::Base
          puts "Would you like to view your user profile? (Y/N)"
          puts
          profile_input = gets.chomp.downcase
-         profile_options(profile_input)
+         profile_selection(profile_input)
     end
 
-    def profile_options(profile_input)
+    def profile_options
+        options_arr = [
+            "View your info.",
+            "Update user name.",
+            "Update your commmute trains.",
+            # "Delete one of your commute trains.", "Add a commute train."
+            "View friends on a commute."
+        ]
+        options_arr.each_with_index { |option, index| puts "#{index + 1}. #{option}"}
+        options_arr
+    end
+
+    def update_first_name
+        puts "Would you like to update your first name? (Y/N)"
+        input = gets.chomp.downcase
+        if input == "y" || input == "yes"
+            puts "Please enter your new first name."
+            new_first_name = gets.chomp.capitalize
+            self.update(first_name: "#{new_first_name}" )
+        elsif input =="n" || input == "no"
+            return
+        end  
+    end
+
+    def update_last_name
+        puts "Would you like to update your last name? (Y/N)"
+        input = gets.chomp.downcase
+        if input == "y" || input == "yes"
+            puts "Please enter your new last name."
+            new_last_name = gets.chomp.capitalize
+            self.update(last_name: "#{new_last_name}" )
+        elsif input =="n" || input == "no"
+            view_profile?
+        end
+    end
+
+
+    def profile_function(option_number, options_arr)
+        if option_number == "1"
+            puts
+            puts "Your full name:"
+            puts "#{self.full_name}"
+            puts
+            puts "Here are the trains you've taken:"
+            self.trains.each { |train| puts "#{train.line}"}
+            puts
+            view_profile?
+        elsif option_number == "2"
+            update_first_name
+            update_last_name
+        elsif option_number == "3"
+            delete_commute_train
+            add_commute_train
+        elsif option_number == "4"
+            puts
+            self.commutes.each do |commute|
+                puts "These are your friends that take the #{commute.train.line}:"
+                puts
+                commute.train.users.each {|user| puts "#{user.full_name}"}
+                puts
+            end
+        else
+            puts "Invalid input. Please try again."
+            new_option_number = gets.chomp
+            profile_function(new_option_number, options_arr)
+        end
+    end
+
+    def profile_selection(profile_input)
         if profile_input == "y" || profile_input == "yes"
             puts
-            puts "Great, #{self.first_name}. What would you like to do?"
-            # update user name
-            # view commute trains
-            # update commute trains
-            # delete commute trains
-            # add commute train
-            # view friends on your commute
+            puts "Great, #{self.first_name}. What would you like to do? (#)"
+            options_arr = profile_options
+            option_number = gets.chomp
+            profile_function(option_number, options_arr)
             exit
         elsif profile_input == "n" || profile_input == "no"
             puts
